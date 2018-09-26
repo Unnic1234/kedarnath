6cd693583fb143892f37c8c9619e1800937cfb98
Unnic1234 <43023276+Unnic1234@users.noreply.github.com>
2018-09-26 14:23:17 +0530

    Update rand6.py

--- rand6.py
+++ rand6.py
@@ -1,3 +1,34 @@
 import random
 r=random.randint(1,6)
 print(r)
+
+OUTPUT
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+2
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+3
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+1
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+2
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+2
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+4
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+3
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+6
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+1
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+1
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+6
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+4
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+6
+dl220@soetcse:~/kedarnath$ python3 rand6.py
+4
+dl220@soetcse:~/kedarnath$ python3 rand6.py
