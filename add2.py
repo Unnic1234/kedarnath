ebb1cf554ad0ac09858baa84a498f2e173c0f202
Unnic1234 <43023276+Unnic1234@users.noreply.github.com>
2018-10-03 14:39:00 +0530

    Update add2.py

--- add2.py
+++ add2.py
@@ -3,3 +3,13 @@ b=(int(input("enter the value of b")))
 #add a and b
 c=a+b
 print(c)
+
+OUTPUT
+dl220@soetcse:~/kedarnath$ python3 add2.py
+enter the value of a5
+enter the value of b6
+11
+dl220@soetcse:~/kedarnath$ python3 add2.py
+enter the value of a45
+enter the value of b58
+103
