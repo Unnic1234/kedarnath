793a25601efae75a21017e5320661fb16ebc4ddb
Unnic1234 <43023276+Unnic1234@users.noreply.github.com>
2018-10-03 14:37:48 +0530

    Delete add3.py

--- add3.py
+++
@@ -1,8 +0,0 @@
-a=(input("enter the value of a: ")
-b=(input("enter the value of b: ")
-if(a>b):
-	print(a,"is greater than",b)
-elif(a==b) :
-	print(a,"is eqaul to",b)
-else(a<b) :
-	print(b,"is greater than",a)
