d18db68b12639dc591f8db88b70b78161df25efc
Unnic1234 <43023276+Unnic1234@users.noreply.github.com>
2018-10-03 14:34:34 +0530

    Update addd8.py

--- addd8.py
+++ addd8.py
@@ -6,3 +6,10 @@ elif(b>a):
  	print(b,"is greater than",a)
 elif(a==b):
 	print(a,"is equal to ",b) 
+	
+	
+OUTPUT
+dl220@soetcse:~/kedarnath$ python3 addd8.py
+enter a value a:8
+enter a value b:8
+8 is equal to  8
