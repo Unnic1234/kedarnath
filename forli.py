ba3a89e8c9d5cb34d2be09ebb57f4e2cdcc72014
Unnic1234 <43023276+Unnic1234@users.noreply.github.com>
2018-10-03 14:32:10 +0530

    Update forli.py

--- forli.py
+++ forli.py
@@ -1,3 +1,23 @@
 import random
 r=random.randint(1,10)
 print(r)
+
+OUTPUT
+dl220@soetcse:~/kedarnath$ python3 forli.py
+3
+dl220@soetcse:~/kedarnath$ python3 forli.py
+4
+dl220@soetcse:~/kedarnath$ python3 forli.py
+1
+dl220@soetcse:~/kedarnath$ python3 forli.py
+2
+dl220@soetcse:~/kedarnath$ python3 forli.py
+5
+dl220@soetcse:~/kedarnath$ python3 forli.py
+5
+dl220@soetcse:~/kedarnath$ python3 forli.py
+6
+dl220@soetcse:~/kedarnath$ python3 forli.py
+10
+dl220@soetcse:~/kedarnath$ python3 forli.py
+4
