c8a8e9dfe28f66b2f9c565c5d56db37bddf2ba75
Unnic1234 <43023276+Unnic1234@users.noreply.github.com>
2018-10-03 14:15:53 +0530

    Update soccer.py

--- soccer.py
+++ soccer.py
@@ -6,3 +6,10 @@ elif(b>a):
  	print(b,"is greater than",a)
 elif(a==b):
 	print(a,"is equal to ",b)
+	
+	OUTPUT
+	
+dl220@soetcse:~/kedarnath$ python3 soccer.py
+enter a value a:5
+enter a value b:6
+6 is greater than 5
