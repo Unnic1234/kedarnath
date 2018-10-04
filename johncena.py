d15b0187ca6920e066e1bfdb450c62946d071039
Unnic1234 <43023276+Unnic1234@users.noreply.github.com>
2018-10-04 10:19:45 +0530

    Update johncena.py

--- johncena.py
+++ johncena.py
@@ -1,3 +1,5 @@
+PLAYING ROCK PAPER SCISSOR WITH COMPUTER
+
 from random import randint
 #create a list of play options
 t = ["R", "P", "S"]
@@ -31,3 +33,34 @@ while player == False:
     #player was set to True, but we want it to be False so the loop continues
     player = False
     computer = t[randint(0,2)]
+    
+OUTPUT
+
+
+R, P, S?R
+Tie!
+R, P, S?
+R, P, S?
+R, P, S?S   
+You lose... R smashes S
+R, P, S?S
+Tie!
+R, P, S?S
+Tie!
+R, P, S?S
+Tie!
+R, P, S?S
+You win! S cut P
+R, P, S?R
+Tie!
+R, P, S?R
+Tie!
+R, P, S?R
+You lose! P covers R
+R, P, S?P
+Tie!
+R, P, S?P
+Tie!
+R, P, S?P
+Tie!
+R, P, S?
